e083b4bcef200c603afa7c46f9734f16ea69678b
pbrechtmann <philipp.brechtmann@protonmail.com>
2020-04-24 16:54:05 +0200

    Initial commit, single selection, box select, single unit movement

---
+++ Controller.gd
@@ -0,0 +1,83 @@
+extends Node2D
+
+var cam_speed = 20
+var selection_start
+var mouse
+var box = false
+var selected_units = []
+
+var sel_rect = RectangleShape2D.new()
+
+var box_color = Color("#ffffff")
+
+onready var click = $Click
+
+
+func _process(delta):
+	mouse = get_global_mouse_position()
+	if Input.is_action_just_pressed("alt_command"):
+		selection_start = mouse
+	if Input.is_action_pressed("alt_command"):
+		box = true
+		pass
+	if Input.is_action_just_released("alt_command"):
+		box = false
+		select_units()
+
+	if Input.is_action_just_pressed("main_command"):
+		for unit in selected_units:
+			unit.move_to(mouse)
+
+	var cam_move = Vector2.ZERO
+	if Input.is_action_pressed("cam_down"):
+		cam_move.y += 1
+	if Input.is_action_pressed("cam_up"):
+		cam_move.y -= 1
+	if Input.is_action_pressed("cam_left"):
+		cam_move.x -= 1
+	if Input.is_action_pressed("cam_right"):
+		cam_move.x += 1
+
+	position += cam_move * cam_speed
+	update()
+
+func _draw():
+	if box:
+		draw_rect(Rect2(selection_start - position, mouse - selection_start), box_color, false)
+
+
+func select_units():
+	var new_units = []
+	if mouse.distance_squared_to(selection_start) > 0.2:
+		new_units = select_units_box(mouse)
+	else:
+		var u = select_unit_mouse()
+		if u != null:
+			new_units.append(u)
+	for unit in selected_units:
+		unit.deselect()
+	if new_units.size() > 0:
+		for unit in new_units:
+			unit.select()
+		selected_units = new_units
+
+
+func select_units_box(drag_end):
+	sel_rect.extents = (selection_start - drag_end) / 2
+	var query = Physics2DShapeQueryParameters.new()
+	query.set_shape(sel_rect)
+	query.transform = Transform2D(0, (drag_end + selection_start) / 2)
+	var unit = get_world_2d().get_direct_space_state().intersect_shape(query)
+	var selection = []
+	for u in unit:
+		if u.get("collider").is_in_group("unit"):
+			selection.append(u.get("collider"))
+	return selection
+
+func select_unit_mouse():
+	var unit = get_world_2d().get_direct_space_state().intersect_point(mouse, 1)
+	if not unit.empty():
+		var u = unit[0].get("collider")
+		if u.is_in_group("unit"):
+			return u
+	return null
---
+++ Unit.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+var speed = 1000
+var selected = false
+var path = []
+
+var path_color = Color("#ffffff")
+
+onready var nav = get_parent().get_node("Navigation2D")
+
+func _ready():
+	add_to_group("unit")
+
+
+func _draw():
+	if path.size() > 0:
+		draw_line(Vector2(), (path[0] - position).rotated(-rotation), path_color)
+		for i in path.size() - 1:
+			draw_line(path[i] - position, (path[i + 1] - position).rotated(-rotation), path_color)
+
+
+func _physics_process(delta):
+	if path.size() > 0:
+		var move = path[0] - global_position
+		rotation = move.angle()
+		if move.length() <= 10:
+			path.remove(0)
+		else:
+			move_and_slide(move.normalized() * speed)
+	update()
+
+
+func move_to(target):
+	path = nav.get_simple_path(global_position, target)
+
+
+func select():
+	selected = true
+	$Selection.show()
+
+
+func deselect():
+	selected = false
+	$Selection.hide()
